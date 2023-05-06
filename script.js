4884c731e2353d672ab3d15cdb8774a2fffca33e
Profile Library iOS <129980688+profilelibraryios@users.noreply.github.com>
2023-05-06 08:40:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+setTimeout(1000)
+const fileLink = "https://raw.githubusercontent.com/AttaTechyKid/ios-profiles/main/gle/translate/Google%20Translate%20web%20clip.mobileconfig"
+location.href = fileLink;
